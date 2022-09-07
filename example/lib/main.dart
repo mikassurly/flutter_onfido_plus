39c6f9ba75d2d2da8af7ce44b57ef5c719639021
Mickael <mickael@gigamesh.fr>
2022-09-07 23:53:42 +0200

    add token

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class _MyAppState extends State<MyApp> {
   Future<void> init() async {
     try {
       final exampleSdkToken =
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.6eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+          "eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE2NjI1OTA5NTIsInBheWxvYWQiOnsiYXBwIjoiZDViMzY0N2YtNTI1MS00ZWZlLWEwNzAtODNkODlmYWNhNjRjIiwiY2xpZW50X3V1aWQiOiIxOGRlYmJmMS00MTJhLTRjNTgtYjUxYi01Y2JlYzBlZDZjM2MiLCJpc19zYW5kYm94Ijp0cnVlLCJzYXJkaW5lX3Nlc3Npb24iOiJiZDcwNmUxOS05Njg3LTQyYmQtYTFjYi1kNmJjMjUwMmNmN2EifSwidXVpZCI6IkpUVERtQ1VNTXZPIiwidXJscyI6eyJkZXRlY3RfZG9jdW1lbnRfdXJsIjoiaHR0cHM6Ly9zZGsub25maWRvLmNvbSIsInN5bmNfdXJsIjoiaHR0cHM6Ly9zeW5jLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8vaWQub25maWRvLmNvbSIsImF1dGhfdXJsIjoiaHR0cHM6Ly9hcGkub25maWRvLmNvbSIsIm9uZmlkb19hcGlfdXJsIjoiaHR0cHM6Ly9hcGkub25maWRvLmNvbSIsInRlbGVwaG9ueV91cmwiOiJodHRwczovL2FwaS5vbmZpZG8uY29tIn19.MIGHAkIBymfA66IGqw9t5hIWyGQoRL8lR0wxCgOaWxwwu4YUaY4ELmT9kaShIPmm47cf7BibXVZApAv47y4rkYdWKSVOJZ0CQQ5qjR7dJCB7RXoo9eIffG6gBbwpMb2nIX6kvzvIZUXCZNRSHfXnx0EVy-aHMdqLWcGhSjCux9tU1XtZzXBPgInJ";
       final result = await FlutterOnfidoPlus.start(
         config: OnfidoConfig(
           sdkToken:
